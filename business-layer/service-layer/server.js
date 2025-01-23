c0685f7989df1f6d22fedce10ca3beb2afb3e6c2
maija-philip <maija.philip@gmail.com>
2025-01-23 10:38:35 -0500

    serves react site + REST api

--- business-layer/service-layer/server.js
+++ business-layer/service-layer/server.js
@@ -9,33 +9,14 @@ const app = express();
 
 // Middleware
 app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(logger('dev'));
-// app.use(express.static(path.join(__dirname, '../../frontend-layer/build')));
-
-// This code makes sure that any request that does not matches a static file
-// in the build folder, will just serve index.html. Client side routing is
-// going to make sure that the correct content will be loaded.
-// https://leejjon.medium.com/create-a-react-app-served-by-express-js-node-js-and-add-typescript-33705be3ceda
-app.use((req, res, next) => {
-    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
-        next();
-    } else {
-        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-        res.header('Expires', '-1');
-        res.header('Pragma', 'no-cache');
-        res.sendFile(path.join(__dirname, '../../frontend-layer/build', 'index.html'));
-    }
-});
-app.use(express.static(path.join(__dirname, '../../frontend-layer/build')));
-
-// hello route
-// app.use((req, res) => {
-//     res.status(200).json({ message: "Hello Tester" });
-// });
+app.use(express.json()); // process JSON
+app.use(express.urlencoded()); // processes form data
+app.use(logger('dev')); // logs out the calls made in dev mode
+app.use(express.static(path.join(__dirname, '../../frontend-layer/build'))); // shares front end statically
 
 // import routes
+let serveFrontendRouter = require("./routes/serveFrontendRoute.js");
+
 let testRouter = require("./routes/testRoute.js");
 let memberRouter = require("./routes/memberRoute.js");
 let organizationRouter = require("./routes/organizationRouter.js");
@@ -44,13 +25,13 @@ let organizationReportsRouter = require("./routes/organizationReportsRouter.js")
 
 
 // use the routes
+app.use("/", serveFrontendRouter);
+
 app.use("/v1/test", testRouter);
 app.use("/v1/member", memberRouter)
 app.use("/v1/organization/:orgId", organizationRouter);
 app.use("/v1/organization/:orgId/member", organizationMemberRouter);
 app.use("/v1/organization/:orgId/reports", organizationReportsRouter);
-
-
 
 // Start the server
 const PORT = process.env.PORT || 8080;
