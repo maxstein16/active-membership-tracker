bbc6c9dd7abb1f5d12d2fef424acf71b31ea1596
aze6809 <aze6809@rit.edu>
2025-01-28 14:28:26 -0500

    Added methods to EDIT organization info

--- business-layer/data-layer/organization.js
+++ business-layer/data-layer/organization.js
@@ -46,8 +46,63 @@ const getOrganizationById = async (organizationId) => {
     }
   };
 
+/**
+   * Update an existing Organization by its ID
+   * @param {number} organizationId - The ID of the organization
+   * @param {object} updatedOrgInfo - The information about the organization to update
+   * @returns {Promise<boolean>} Returns true if the organization was updated, 
+   *    false if no organization with the provided ID exists
+   */
+const updateOrganizationByID = async (organizationId, updatedOrgInfo) => {
+  try {
+    const [updatedOrgRows] = await Organization.update(updatedOrgInfo, {
+      where: { organization_id: organizationId }
+    });
+
+    if ( updatedOrgRows > 0) {
+      console.log(`Organization with ID ${organizationId} updated successfully.`);
+      return true;
+    } else {
+      console.log(`No organization found with ID ${organizationId}`);
+      return false;
+    }
+  } catch (err) {
+    console.error("Error updating organization:", err);
+    throw err;
+  }
+};
+
+/**
+   * Update an existing Organization's description by its Name
+   * @param {number} organizationName - The name of the organization
+   * @param {object} updatedOrgDesc - The information about the organization to update
+   * @returns {Promise<boolean>} Returns true if the organization was updated, 
+   *    false if no organization with the provided name exists
+   */
+const updateOrganizationByName = async (organizationName, updatedOrgDesc) => {
+  try {
+    const [updatedOrgRows] = await Organization.update(updatedOrgDesc, {
+      where: { organization_name: organizationName }
+    });
+
+    if ( updatedOrgRows > 0) {
+      console.log(`Organization with name ${organizationName} updated successfully.`);
+      return true;
+    } else {
+      console.log(`No organization found with name ${organizationName}`);
+      return false;
+    }
+  } catch (err) {
+    console.error("Error updating organization:", err);
+    throw err;
+  }
+};
+
+
 module.exports = {
     createOrganization,
     getOrganizationById,
-    getOrganizations
+    getOrganizations,
+    updateOrganizationByID,
+    updateOrganizationByName
 }
