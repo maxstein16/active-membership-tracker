565d885f2d67114d8d06f593a02a6fd20f727ce4
maija-philip <maija.philip@gmail.com>
2025-01-28 15:09:19 -0500

    finish rest of routes

--- business-layer/business-logic-layer/public/exports.js
+++ business-layer/business-logic-layer/public/exports.js
@@ -1,4 +1,4 @@
-const { getSpecificMemberWithOrgData, addMemberToAnOrganization } = require("../organizationMemberProcessing");
+const { getSpecificMemberWithOrgData, addMemberToAnOrganization, editMemberInOrganization } = require("../organizationMemberProcessing");
 
 // export to api calls
 module.exports = function () {
@@ -10,6 +10,14 @@ module.exports = function () {
 
   this.addMemberToOrg = async (orgId, memberData) => {
     return await addMemberToAnOrganization(orgId, memberData);
+  }
+
+  this.editMemberInOrg = async (orgId, memberId, memberDataToUpdate) => {
+    return await editMemberInOrganization(orgId, memberId, memberDataToUpdate);
+  }
+
+  this.deleteMemberInOrg = async (orgId, memberId) => {
+    return await editMemberInOrganization(orgId, memberId);
   }
 
   // Member Calls (not org specific)
