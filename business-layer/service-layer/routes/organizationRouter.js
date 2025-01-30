fd244e3e879bdbf7db1a3513429486ea4d63ccec
aze6809 <aze6809@rit.edu>
2025-01-30 17:58:34 -0500

    Added PUT method response (syntax not checked)

--- business-layer/service-layer/routes/organizationRouter.js
+++ business-layer/service-layer/routes/organizationRouter.js
@@ -42,6 +42,27 @@ router.post("/", function (req, res) {
     res.status(404).json({ error: "organization with id of {orgId} not found" });
 
     res.status(500).json({ error: "Something went wrong" });
+});
+
+//PUT /v1/organization/{orgId}
+router.put("/", function (req, res) {
+  res.status(200).json({
+      "status": "success",
+      "data": {
+        "organization_id": 1,
+        "organization_name": "Women In Computing",
+        "organization_abbreviation": "WiC",
+        "organization_desc": "Our Mission is to build a supportive community that celebrates the talent of underrepresented students in Computing. We work to accomplish our mission by providing mentorship, mental health awareness, and leadership opportunities.",
+        "organization_color": 0x123456,
+        "active_membership_threshold": 48 
+        }
+    });
+
+  res.status(400).json({ error: "Must include at least one valid feild to edit: organization_name, organization_abbreviation, organization_desc, organization_color, active_membership_threshold"});
+
+  res.status(404).json({ error: "organization with id of {orgId} not found" });
+
+  res.status(500).json({  error: "Something went wrong" });
 
 });
 
