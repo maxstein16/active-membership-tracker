ab2226aeedeb95817dcdf9fdf9ffb89ef133d3c6
Divna Mijic <dm9718@rit.edu>
2025-02-01 14:56:21 -0500

    Encryption of password when adding member to organization in exports.js BLL

--- business-layer/business-logic-layer/public/exports.js
+++ business-layer/business-logic-layer/public/exports.js
@@ -1,4 +1,9 @@
-const { getSpecificMemberWithOrgData, addMemberToAnOrganization, editMemberInOrganization } = require("../organizationMemberProcessing");
+const {
+  getSpecificMemberWithOrgData,
+  addMemberToAnOrganization,
+  editMemberInOrganization,
+} = require("../organizationMemberProcessing");
+const bcrypt = require("bcryptjs"); // or 'bcrypt' if you prefer
 
 // export to api calls
 module.exports = function () {
@@ -9,22 +14,27 @@ module.exports = function () {
   };
 
   this.addMemberToOrg = async (orgId, memberData) => {
+    // Hashing the password before storing it
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(memberData.password, saltRounds);
+    memberData.password = hashedPassword; // Replacing the plain password with the newly hashed one
+    // Passing the memberData with the hashed password to the Data Access Layer
     return await addMemberToAnOrganization(orgId, memberData);
-  }
+  };
 
   this.editMemberInOrg = async (orgId, memberId, memberDataToUpdate) => {
     return await editMemberInOrganization(orgId, memberId, memberDataToUpdate);
-  }
+  };
 
   this.deleteMemberInOrg = async (orgId, memberId) => {
     return await editMemberInOrganization(orgId, memberId);
-  }
+  };
 
   // Member Calls (not org specific)
 
   // Organization Calls
 
   // Organization Settings Calls
-  
+
   // Organization Reports Calls
 };
