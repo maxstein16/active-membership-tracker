0951b13c527b2c094f3ce131badba9f2d1ff32c7
Divna Mijic <dm9718@rit.edu>
2025-02-01 14:33:23 -0500

    removing unnecessary DB retrievals of Member in BLL, memberProcessing.js

--- business-layer/business-logic-layer/memberProcessing.js
+++ business-layer/business-logic-layer/memberProcessing.js
@@ -37,53 +37,6 @@ async function getSpecificMemberData(memberId) {
   // TODO: Implement database call to fetch member data.
   return { error: error.noError, data: "data-here" };
 }
-
-/**
- * Adds a new member to the database.
- *
- * @param {Object} memberData - The data of the new member.
- * @returns {Object} - A response containing either an error or the newly added member's data.
- *
- * Example response format:
- * (Same as getSpecificMemberData)
- */
-async function addMember(memberData) {
-  // TODO: Implement database call to add a new member.
-  return { error: error.noError, data: "data-here" };
-}
-
-/**
- * Edits an existing member's information in the database.
- *
- * @param {number} memberId - The ID of the member to update.
- * @param {Object} memberDataToUpdate - The fields to update for the member.
- * @returns {Object} - A response containing either an error or the updated member's data.
- *
- * Example response format:
- * (Same as getSpecificMemberData)
- */
-async function editMember(memberId, memberDataToUpdate) {
-  // TODO: Implement database call to update member data.
-  return { error: error.noError, data: "data-here" };
-}
-
-/**
- * Deletes a member from the database.
- *
- * @param {number} memberId - The ID of the member to delete.
- * @returns {Object} - A response confirming the deletion or an error message.
- *
- * Note:
- * - We are not actually deleting any member data at this point.
- * - Instead, consider marking the member as inactive or archiving their data.
- */
-async function deleteMember(memberId) {
-  // TODO: Implement soft delete or archiving logic instead of actual deletion.
-}
-
 module.exports = {
   getSpecificMemberData,
-  addMember,
-  editMember,
-  deleteMember,
 };
