9d3e96b89cbe4575ee60bd7427883230778597aa
maija-philip <maija.philip@gmail.com>
2025-02-18 15:20:42 -0500

    more test data

--- business-layer/__tests__/addTestData.js
+++ business-layer/__tests__/addTestData.js
@@ -4,9 +4,21 @@
  * HOW TO RUN
  * cd ./active-membership-tracker/business-layer
  * node  ./__tests__/addTestData.js
+ *
+ * WARNING TO FUTURE DEVELOPERS:
+ * The members are real people with real RIT usernames.
+ * We as the initial developers are using our own data to test.
+ * If you run this on the production site, you will delete all our data....
  */
 
-const { Organization, Semester, Member } = require("../db");
+const {
+  Organization,
+  Semester,
+  Member,
+  Membership,
+  EmailSettings,
+  MembershipRequirement,
+} = require("../db");
 const { deleteOldTestData } = require("./deleteOldTestData");
 
 async function addTestData() {
@@ -15,10 +27,10 @@ async function addTestData() {
   await deleteOldTestData();
 
   console.log("\n\nAdding test data to Database...");
-  
+
   // ORGANIZATION
   console.log("\n1. Creating Test Organizations");
-  await Organization.create({
+  const org1 = await Organization.create({
     organization_name: "WiC TEST ONLY",
     organization_description: "This is a test for WiC Settings",
     organization_color: "#381A58",
@@ -26,7 +38,7 @@ async function addTestData() {
     organization_threshold: 42,
   });
 
-  await Organization.create({
+  const org2 = await Organization.create({
     organization_name: "COMS TEST ONLY",
     organization_description: "This is a test for COMS Settings",
     organization_color: "#20BDE4",
@@ -37,7 +49,7 @@ async function addTestData() {
   // SEMESTERS
   console.log("2. Creating Test Semesters");
 
-  await Semester.create({
+  const sem1 = await Semester.create({
     semester_id: 1123,
     semester_name: "2024 FALL TEST",
     academic_year: "2024-2025",
@@ -45,7 +57,7 @@ async function addTestData() {
     end_date: "2024-12-16",
   });
 
-  await Semester.create({
+  const sem2 = await Semester.create({
     semester_id: 1124,
     semester_name: "2025 SPRING TEST",
     academic_year: "2024-2025",
@@ -56,7 +68,7 @@ async function addTestData() {
   // MEMBERS
   console.log("3. Creating Test Members");
 
-  await Member.create({
+  const member1 = await Member.create({
     member_name: "Maija Philip",
     member_email: "mep4741@rit.edu",
     member_personal_email: "maija.philip@gmail.com",
@@ -67,7 +79,7 @@ async function addTestData() {
     member_race: "White",
   });
 
-  await Member.create({
+  const member2 = await Member.create({
     member_name: "Max Stein",
     member_email: "mhs8558@rit.edu",
     member_personal_email: "max.stein@gmail.com",
@@ -78,7 +90,7 @@ async function addTestData() {
     member_race: "White",
   });
 
-  await Member.create({
+  const member3 = await Member.create({
     member_name: "Divna Mijic",
     member_email: "dm9718@rit.edu",
     member_personal_email: "divna.mijic@gmail.com",
@@ -89,7 +101,7 @@ async function addTestData() {
     member_race: "White",
   });
 
-  await Member.create({
+  const member4 = await Member.create({
     member_name: "Kasim O'Meally",
     member_email: "klo7619@rit.edu",
     member_personal_email: "kasim.omeally@gmail.com",
@@ -97,7 +109,7 @@ async function addTestData() {
     member_major: "Web and Mobile Computing",
   });
 
-  await Member.create({
+  const member5 = await Member.create({
     member_name: "Joseph Henry",
     member_email: "jdh6459@rit.edu",
     member_personal_email: "joseph.henry@gmail.com",
@@ -105,7 +117,7 @@ async function addTestData() {
     member_major: "Web and Mobile Computing",
   });
 
-  await Member.create({
+  const member6 = await Member.create({
     member_name: "Alexandria Eddings",
     member_email: "aze6809@rit.edu",
     member_personal_email: "alexandria.eddings@gmail.com",
@@ -113,13 +125,194 @@ async function addTestData() {
     member_major: "Web and Mobile Computing",
   });
 
-  await Member.create({
+  const member7 = await Member.create({
     member_name: "Gabriella Alvarez-Mcleod",
     member_email: "gma5228@rit.edu",
     member_personal_email: "Gabriella.alvarez-mcleod@gmail.com",
     member_graduation_date: "2025-05-10",
     member_major: "Human Computer Interaction",
   });
+
+  // Memberships
+  console.log("4. Creating Test Memberships");
+
+  const members = [
+    member1,
+    member2,
+    member3,
+    member4,
+    member5,
+    member6,
+    member7,
+  ];
+
+  for (const member of members) {
+    let randomNum = Math.floor(Math.random() * 40) + 1;
+    const membership1a = await Membership.create({
+      membership_role: 2,
+      member_id: member.member_id,
+      organization_id: org1.organization_id,
+      semester_id: sem1.semester_id,
+      membership_points: randomNum,
+      active_member: randomNum >= 42,
+      active_semesters: 1,
+    });
+
+    randomNum = Math.floor(Math.random() * 40) + 1;
+    const membership1b = await Membership.create({
+      membership_role: 2,
+      member_id: member.member_id,
+      organization_id: org1.organization_id,
+      semester_id: sem1.semester_id,
+      membership_points: randomNum,
+      active_member: randomNum >= 23,
+      active_semesters: 1,
+    });
+  }
+
+  // Email Settings
+  console.log("5. Adding Email Settings");
+
+  await EmailSettings.create({
+    organization_id: org1.organization_id,
+    current_status: true,
+    annual_report: false,
+    semester_report: true,
+    membership_achieved: false,
+  });
+
+  await EmailSettings.create({
+    organization_id: org2.organization_id,
+    current_status: false,
+    annual_report: false,
+    semester_report: true,
+    membership_achieved: true,
+  });
+
+  // Membership Requirements
+  console.log("6. Adding Membership Requirements");
+
+  await MembershipRequirement.create({
+    organization_id: org1.organization_id,
+    meeting_type: "Meeting",
+    frequency: "Semesterly",
+    amount_type: "points",
+    amount: 4,
+    requirement_scope: "semesterly",
+  });
+
+  await MembershipRequirement.create({
+    organization_id: org1.organization_id,
+    meeting_type: "Event",
+    frequency: "Yearly",
+    amount_type: "points",
+    amount: 2,
+    requirement_scope: "yearly",
+  });
+
+  await MembershipRequirement.create({
+    organization_id: org1.organization_id,
+    meeting_type: "Volunteer",
+    frequency: "Semesterly",
+    amount_type: "percentage",
+    amount: 50,
+    requirement_scope: "semesterly",
+  });
+
+  await MembershipRequirement.create({
+    organization_id: org2.organization_id,
+    meeting_type: "Meeting",
+    frequency: "Yearly",
+    amount_type: "points",
+    amount: 1,
+    requirement_scope: "yearly",
+  });
+
+  await MembershipRequirement.create({
+    organization_id: org2.organization_id,
+    meeting_type: "Event",
+    frequency: "Semesterly",
+    amount_type: "percentage",
+    amount: 25,
+    requirement_scope: "semesterly",
+  });
+
+  await MembershipRequirement.create({
+    organization_id: org2.organization_id,
+    meeting_type: "Volunteer",
+    frequency: "Semesterly",
+    amount_type: "percentage",
+    amount: 50,
+    requirement_scope: "semesterly",
+  });
+
+  // Events
+  console.log("7. Adding Events");
+  await Event.create({
+    organization_id: org1.organization_id,
+    event_name: "WiC General Meeting",
+    event_start: "2025-02-05 18:00:00",
+    event_end: "2025-02-05 19:30:00",
+    event_location: "GOL 1400",
+    event_description: "An overview of upcoming events and initiatives.",
+    event_type: "general_meeting",
+  });
+  await Event.create({
+    organization_id: org1.organization_id,
+    event_name: "WiC Volunteer Day",
+    event_start: "2025-03-10 10:00:00",
+    event_end: "2025-03-10 14:00:00",
+    event_location: "Local Community Center",
+    event_description: "Helping out at the community center with tech workshops.",
+    event_type: "volunteer",
+  });
+
+  await Event.create({
+    organization_id: org1.organization_id,
+    event_name: "WiC Social Night",
+    event_start: "2025-04-15 19:00:00",
+    event_end: "2025-04-15 22:00:00",
+    event_location: "Java's Café",
+    event_description: "A night of networking, games, and fun!",
+    event_type: "social",
+  },)
+  await Event.create( {
+    organization_id: org2.organization_id,
+    event_name: "COMS Workshop: Resume Building",
+    event_start: "2025-02-12 17:30:00",
+    event_end: "2025-02-12 19:00:00",
+    event_location: "GOL 2250",
+    event_description: "Learn how to craft a compelling resume with industry professionals.",
+    event_type: "workshop",
+  },)
+  await Event.create({
+    organization_id: org2.organization_id,
+    event_name: "COMS Charity Fundraiser",
+    event_start: "2025-03-20 18:00:00",
+    event_end: "2025-03-20 21:00:00",
+    event_location: "RIT Ballroom",
+    event_description: "A night of fundraising for a local cause with guest speakers.",
+    event_type: "fundraiser",
+  },)
+  await Event.create({
+    organization_id: org2.organization_id,
+    event_name: "COMS Committee Brainstorming",
+    event_start: "2025-04-10 16:00:00",
+    event_end: "2025-04-10 18:00:00",
+    event_location: "GOL 3000",
+    event_description: "Collaborate and plan initiatives for the next semester.",
+    event_type: "committee",
+  })
+
+
+  // Events
+  console.log("8. Adding Attendance");
+
+  // Finishing up
+  console.log("\nFinishing up...");
+  console.log("This may take a couple minutes...wait for the script to end");
 }
 
-(async () => { addTestData()})();
+(async () => {
+  addTestData();
+})();
--- business-layer/__tests__/deleteOldTestData.js
+++ business-layer/__tests__/deleteOldTestData.js
@@ -1,24 +1,55 @@
-const { Organization, Semester, Member } = require("../db");
+const { Organization, Semester, Member, Membership, EmailSettings, MembershipRequirement } = require("../db");
 
 // RUN WITH ADD TEST DATA
 async function deleteOldTestData() {
-  // delete organizations
-  let result = await Organization.destroy({
+
+  let result = undefined;
+
+  // get Org Ids
+  let org1Id = await Organization.findOne({
     where: { organization_name: "WiC TEST ONLY" },
   });
+  org1Id = org1Id.organization_id;
 
-  result = await Organization.destroy({
+  let org2Id = await Organization.findOne({
     where: { organization_name: "COMS TEST ONLY" },
   });
+  org2Id = org2Id.organization_id;
 
-  // delete semesters
-  result = await Semester.destroy({
-    where: { semester_name: "2024 FALL TEST" },
-  });
 
-  result = await Semester.destroy({
-    where: { semester_name: "2025 SPRING TEST" },
-  });
+
+  // delete events
+  result = await Event.destroy({
+    where: { organization_id: org1Id }
+  })  
+  result = await Event.destroy({
+    where: { organization_id: org2Id }
+  })  
+
+
+  // delete membership requirements
+  result = await MembershipRequirement.destroy({
+    where: { organization_id: org1Id }
+  })  
+  result = await MembershipRequirement.destroy({
+    where: { organization_id: org2Id }
+  })  
+
+  // delete email settings
+  result = await EmailSettings.destroy({
+    where: { organization_id: org1Id }
+  })  
+  result = await EmailSettings.destroy({
+    where: { organization_id: org2Id }
+  }) 
+
+  // delete memberships
+  result = await Membership.destroy({
+    where: { organization_id: org1Id }
+  })  
+  result = await Membership.destroy({
+    where: { organization_id: org2Id }
+  })  
 
   // delete members
   result = await Member.destroy({
@@ -47,6 +78,24 @@ async function deleteOldTestData() {
 
   result = await Member.destroy({
     where: { member_email: "gma5228@rit.edu" },
+  });
+
+  // delete semesters
+  result = await Semester.destroy({
+    where: { semester_name: "2024 FALL TEST" },
+  });
+
+  result = await Semester.destroy({
+    where: { semester_name: "2025 SPRING TEST" },
+  });
+
+  // delete organizations
+  result = await Organization.destroy({
+    where: { organization_name: "WiC TEST ONLY" },
+  });
+
+  result = await Organization.destroy({
+    where: { organization_name: "COMS TEST ONLY" },
   });
 }
 
