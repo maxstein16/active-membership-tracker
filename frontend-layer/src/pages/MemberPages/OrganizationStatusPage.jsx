8308e70e3e09e6293560ef118460de5b8b42a5c5
aze6809 <aze6809@rit.edu>
2025-03-06 15:37:35 -0500

    Added basic text to org status page

--- frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
+++ frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
@@ -11,8 +11,18 @@ export default function OrganizationStatusPage() {
       <BackButton route={"/"} />
 
       <h1>Organization Status Page</h1>
+      <h1>Your Membership with WiC</h1>
+      <p>You have been an active member for # semesters</p>
+
+      <p>You have earned # total points.</p>
+      <p>Earn # more to become an active member this semester!</p>
       <p>Org Id: {orgId}</p>
       <br />
+<h2>Computing Organization for Multicultural Students</h2>
+<p>Our mission is to build a supportive community that celebrates the talent of underrepresented students in Computing.
+  We work to accomplish our mision by providing mentorship, mental health awareness, and leadership opportunities.
+</p>
+
     </PageSetup>
   );
 }
