5b9525a4ef26a58f6fdaa33d9e9af315c9a40c20
komeally <kasim.omeally@gmail.com>
2025-03-11 19:40:28 -0400

    Check if last admin

--- business-layer/business-logic-layer/constants.js
+++
@@ -1,7 +0,0 @@
-const ROLE_ADMIN = 2;
-const ROLE_EBOARD = 1;
-const ROLE_MEMBER = 0;
-
-module.exports = {
-    ROLE_ADMIN, ROLE_EBOARD, ROLE_MEMBER
-  };
--- business-layer/constants.js
+++ business-layer/constants.js
@@ -1,7 +1,15 @@
+const ROLE_ADMIN = 2;
+const ROLE_EBOARD = 1;
+const ROLE_MEMBER = 0;
 const HOUR_1 = 3_600_000;
 const MIN_30 = 1_800_000;
 const MIN_5 = 300_000;
 
 module.exports = {
-    HOUR_1, MIN_30, MIN_5
-  };
+  ROLE_ADMIN,
+  ROLE_EBOARD,
+  ROLE_MEMBER,
+  HOUR_1,
+  MIN_30,
+  MIN_5,
+};
