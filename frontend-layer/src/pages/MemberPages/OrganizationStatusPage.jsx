eee7b435935595cdc9a930647b6874236f1a98c4
aze6809 <aze6809@rit.edu>
2025-03-16 23:13:19 -0400

    added more tests data

--- frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
+++ frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
@@ -6,39 +6,79 @@ import { API_METHODS, getAPIData } from "../../utils/callAPI";
 
 export default function OrganizationStatusPage() {
   const { orgId } = useParams();
+  let [posts, setPosts] = React.useState([]);
+  let [loading, setLoading] = React.useState(false);
+  let [memberships, setMemberships] = React.useState([]);
+ 
   var lot = useParams();
   //var th = getAPIData("/organization", API_METHODS.get);
-  //console.log(th);
- let bob = async () => {
+  var thing ;
+  async function loadData(){
+    const result = await getAPIData("/organization/1/member/1", API_METHODS.get, {});
+    return {data: result}
+  }
+
+//  let bob = async () => {
     
-    const result = await getAPIData("/organization/1", API_METHODS.get, {});
+//     const result = await getAPIData("/organization/1/member/1", API_METHODS.get, {});
+//   console.log(result);
+//     if (!result) {
+//       return;
+//     }
 
-    if (!result) {
-      return;
-    }
+//     if (result.status == "success") {
+//       console.log(result);
+//       return result;
+//     }
+//   };
 
-    if (result.status === "success") {
-      console.log(result);
-      return;
-    }
-  };
-
+ React.useEffect(() => {
  
+  let loadPost = async () => {
+    setLoading(true);
+    const result = await getAPIData(`/organization/1/member/1`, API_METHODS.get, {});
+    setPosts(result.data);
+
+    setMemberships(result.data.membership);
+    console.log("org id: " + orgId);
+
+    const response = await getAPIData(
+      `/organization/${orgId}/settings`,
+      API_METHODS.get,
+      {}
+    );
+    let threshold = response.data.organization_threshold;
+
+    setLoading(false);
+    console.log(result.data);
+    console.log("break");
+    console.log(threshold);
+  };
+  loadPost();
+
+
+//   console.log("Loaded");
+//   const res = await getAPIData("/organization/1/member/1", API_METHODS.get, {});
+// console.log("res done");
+// console.log(res);
+//   thing = loadData();
+ }, []);
 
 
   return (
     <PageSetup>
       <BackButton route={"/"} />
       {console.log(lot.orgId)}
-    {bob}
-
+      {/* <button onClick={bob}>Ln</button> */}
       <h1>Organization Status Page</h1>
       <h1>Your Membership with WiC</h1>
-      <p>You have been an active member for # semesters</p>
+      <p>You have been an active member for {memberships.active_semesters} semesters</p>
+      
+      <h3>{posts.member_id}</h3>
 
-      <p>You have earned # total points.</p>
+      <p>You have earned {memberships.membership_points} total points.</p>
       <p>Earn # more to become an active member this semester!</p>
-      <p>Org Id: {lot.orgId}</p>
+      {/* <p>Org Id: {bob.data.membership.active_semesters}</p> */}
       <br />
       <h2>Computing Organization for Multicultural Students</h2>
       <p>Our mission is to build a supportive community that celebrates the talent of underrepresented students in Computing.
