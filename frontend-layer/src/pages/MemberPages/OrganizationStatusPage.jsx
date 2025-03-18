dce6cee3368efec0fd9b62dba6f6404d9c20a014
aze6809 <aze6809@rit.edu>
2025-03-18 17:26:50 -0400

    page is now broken for some reason. possibly latest merge?

--- frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
+++ frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
@@ -36,11 +36,11 @@ export default function OrganizationStatusPage() {
     );
     
       setOrgData(response.data);
-      // console.log(result.data);
+     console.log(result.data);
        console.log(response.data);
   
       let data = [
-        {name: 'completed', value: orgData.active_membership_threshold},
+        {name: 'completed', value: orgData.active_organization_threshold},
         {name: 'progress', value: memberships.membership_points}
   
       ];
@@ -62,21 +62,21 @@ export default function OrganizationStatusPage() {
 
       <div className={ windowWidth > 499 ? "member-progress-chart-page" : "member-progress-chart-page-mobile"}>
         <div>
-          <h1>Your Membership with <span style={{color: orgData.org_color}}>{orgData.org_abbreviation}</span></h1>
+          <h1>Your Membership with <span style={{color: orgData.organization_color}}>{orgData.organization_abbreviation}</span></h1>
           
           <p>You have been an active member for <strong>{memberships.active_semesters}</strong> semesters.</p>
           <ResponsiveContainer width="100%" height="50%">
             <PieChart>
             
-              { memberships.active_semesters < orgData.active_membership_threshold &&
+              { memberships.active_semesters < orgData.active_organization_threshold &&
                 <text x='50%' y="40%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
-                <tspan x="50%" fill={orgData.org_color} dy=".6em">  {(parseInt(memberships.membership_points) / parseInt(orgData.active_membership_threshold) * 100).toFixed(0)} %</tspan>
+                <tspan x="50%" fill={orgData.organization_color} dy=".6em">  {(parseInt(memberships.membership_points) / parseInt(orgData.active_organization_threshold) * 100).toFixed(0)} %</tspan>
                 <tspan x="50%" dy="1.2em">to Active</tspan>
               < tspan x="50%" dy="1.2em">Membership</tspan>
                 </text>
               }
 
-              { memberships.active_semesters >= orgData.active_membership_threshold &&
+              { memberships.active_semesters >= orgData.active_organization_threshold &&
                 <text x='50%' y="50%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
                   <tspan x="50%" dy=".6em">Active</tspan>
                   <tspan x="50%" dy="1.2em">Membership</tspan>
@@ -87,23 +87,22 @@ export default function OrganizationStatusPage() {
                 dataKey="value"
                 data={[
                   { name: "Progress", value: memberships.membership_points },
-                  { name: "Completed", value: orgData.active_membership_threshold }
+                  { name: "Completed", value: orgData.active_organization_threshold }
                 ]}
                 
                 cx="50%"
                 cy="50%"
                 innerRadius="50%"
                 outerRadius="80%"
-              
               > 
               {
                 orgId === 1 &&
-                <><Cell fill='#7747a6' /><Cell fill='#7747a6' /></>
+                <><Cell fill='#7747a6' stroke={orgData.organization_color} /><Cell fill='#7747a6' stroke={orgData.organization_color} /></>
               }
               
               {
                 orgId === 2 &&
-                <><Cell fill="#476da6" /><Cell fill="#476da6" /></>
+                <><Cell fill="#476da6" stroke={orgData.organization_color} /><Cell fill="#476da6" /></>
               }
 
             </Pie>
@@ -112,17 +111,17 @@ export default function OrganizationStatusPage() {
 
           {
             memberships.active_member ?  <p>Your total points this semester are <strong>{memberships.membership_points}</strong>! 
-            That is ${(memberships.membership_points - orgData.active_membership_threshold)} points above the minimum requirement!</p>:
+            That is ${(memberships.membership_points - orgData.active_organization_threshold)} points above the minimum requirement!</p>:
             <p>You have {memberships.membership_points} total points. <br/>Earn <strong>{
-              orgData.active_membership_threshold - memberships.membership_points
+              orgData.active_organization_threshold - memberships.membership_points
               }</strong> more to become an active member this semester!</p> 
           }
 
           </div>
       
         <div style={{marginTop: '25%', marginBottom: '25%'}} >
-            <h2>{orgData.org_name}</h2>
-            <p>{orgData.org_description} </p>
+            <h2>{orgData.organization_name}</h2>
+            <p>{orgData.organization_description} </p>
         </div>
       </div>
     </PageSetup>
