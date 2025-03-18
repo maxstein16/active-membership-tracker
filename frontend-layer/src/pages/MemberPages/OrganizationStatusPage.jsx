9b41337c445826db1ce8ef24f0a84c43fea2eb68
aze6809 <aze6809@rit.edu>
2025-03-18 03:46:33 -0400

    added media queries for org status page

--- frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
+++ frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
@@ -35,7 +35,7 @@ export default function OrganizationStatusPage() {
       API_METHODS.get,
       {}
     );
-    let threshold = response.data.active_membership_threshold;
+    
     setOrgData(response.data);
 
    
@@ -68,81 +68,70 @@ export default function OrganizationStatusPage() {
     <PageSetup>
       <BackButton route={"/"} />
       
-      <div style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-        width: '100%',
-        gap: '10%',
-        marginLeft: '10%',
-        marginRight: '10%'
-      }} >
-      <div>
-      <h1>Your Membership with <span style={{color: "#d6d2dd"}}>{orgData.org_name}</span></h1>
-      
-      <p>You have been an active member for <strong>{memberships.active_semesters}</strong> semesters.</p>
-      <ResponsiveContainer width="50%" height={270}>
-        <PieChart>
-        
-        	{ memberships.active_semesters < orgData.active_membership_threshold &&
-            <text x='50%' y="40%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
-            <tspan x="50%" dy=".6em">  {(parseInt(memberships.membership_points) / parseInt(orgData.active_membership_threshold) * 100).toFixed(0)} %</tspan>
-          <tspan x="50%" dy="1.2em">to Active</tspan>
-          <tspan x="50%" dy="1.2em">Membership</tspan>
-            </text>
-          }
-
-          { memberships.active_semesters >= orgData.active_membership_threshold &&
-            <text x='50%' y="50%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
-               <tspan x="50%" dy=".6em">Active</tspan>
-               <tspan x="50%" dy="1.2em">Membership</tspan>
-            </text>
-          }
-        
-        <Pie
-          dataKey="value"
-          data={[
-            { name: "Progress", value: memberships.membership_points },
-            { name: "Completed", value: orgData.active_membership_threshold }
-          ]}
+      <div className="member-progress-chart-page">
+        <div>
+          <h1>Your Membership with <span style={{color: orgData.org_color}}>{orgData.org_abbreviation}</span></h1>
           
-          cx="50%"
-          cy="50%"
-          innerRadius={90}
-          outerRadius={120}
-          label
-        > 
-        {
-           orgId === 1 &&
-          <><Cell fill="#d6d2dd" /><Cell fill="#482659"  /></>
-        }
-         
-         {
-           orgId === 2 &&
-          <><Cell fill= "#d6d2dd" /><Cell fill= "#5ebbdf" /></>
-        }
+          <p>You have been an active member for <strong>{memberships.active_semesters}</strong> semesters.</p>
+          <ResponsiveContainer width="100%" height="50%">
+            <PieChart>
+            
+              { memberships.active_semesters < orgData.active_membership_threshold &&
+                <text x='50%' y="40%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
+                <tspan x="50%" fill={orgData.org_color} dy=".6em">  {(parseInt(memberships.membership_points) / parseInt(orgData.active_membership_threshold) * 100).toFixed(0)} %</tspan>
+              <tspan x="50%" dy="1.2em">to Active</tspan>
+              <tspan x="50%" dy="1.2em">Membership</tspan>
+                </text>
+              }
 
-        </Pie>
-        </PieChart>
-      </ResponsiveContainer>
+              { memberships.active_semesters >= orgData.active_membership_threshold &&
+                <text x='50%' y="50%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
+                  <tspan x="50%" dy=".6em">Active</tspan>
+                  <tspan x="50%" dy="1.2em">Membership</tspan>
+                </text>
+              }
+            
+              <Pie
+                dataKey="value"
+                data={[
+                  { name: "Progress", value: memberships.membership_points },
+                  { name: "Completed", value: orgData.active_membership_threshold }
+                ]}
+                
+                cx="50%"
+                cy="50%"
+                innerRadius="50%"
+                outerRadius="80%"
+                label
+              > 
+              {
+                orgId === 1 &&
+                <><Cell fill="#d6d2dd" /><Cell fill={orgData.org_color}  /></>
+              }
+              
+              {
+                orgId === 2 &&
+                <><Cell fill= "#d6d2dd" /><Cell fill={orgData.org_color} /></>
+              }
 
-      {
-        memberships.active_member ?  <p>Your total points this semester are <strong>{memberships.membership_points}</strong>! That is ${(memberships.membership_points - orgData.active_membership_threshold)} points above the minimum requirement!</p>:
-        <p>You have {memberships.membership_points} total points. <br/>Earn <strong>{
-          orgData.active_membership_threshold - memberships.membership_points
-          }</strong> more to become an active member this semester!</p> 
-    }
+            </Pie>
+            </PieChart>
+          </ResponsiveContainer>
 
-      </div>
-     
-      <div style={{
-      marginTop: '25%',
-     marginBottom: '25%'
-      }} >
-       
-          <h2>{orgData.org_name}</h2>
-          <p>{orgData.org_description} </p>
-       
-      </div>
+          {
+            memberships.active_member ?  <p>Your total points this semester are <strong>{memberships.membership_points}</strong>! 
+            That is ${(memberships.membership_points - orgData.active_membership_threshold)} points above the minimum requirement!</p>:
+            <p>You have {memberships.membership_points} total points. <br/>Earn <strong>{
+              orgData.active_membership_threshold - memberships.membership_points
+              }</strong> more to become an active member this semester!</p> 
+          }
+
+          </div>
+      
+        <div style={{marginTop: '25%', marginBottom: '25%'}} >
+            <h2>{orgData.org_name}</h2>
+            <p>{orgData.org_description} </p>
+        </div>
       </div>
     </PageSetup>
   );
