ef9c688273fc74abc889c027b0a3214a09214349
aze6809 <aze6809@rit.edu>
2025-03-20 16:31:11 -0400

    commented out console.log statements

--- frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
+++ frontend-layer/src/pages/MemberPages/OrganizationStatusPage.jsx
@@ -62,14 +62,13 @@ export default function OrganizationStatusPage() {
 
       <div className={ windowWidth > 499 ? "member-progress-chart-page" : "member-progress-chart-page-mobile"}>
         <div>
-          <h1>Your Membership with {orgData.organization_abbreviation}</h1>
-          {
+          <h1>Your Membership with <span style={{color: orgData.organization_color}}>{orgData.organization_abbreviation}</span></h1>
+          {/* {
             console.log(orgData)
           }
           {console.log(membershipInfo)
-          }
+          } */}
 
-          {console.log(memberInfo)}
           <p>You have been an active member for <strong>{memberInfo.activeSemesters} </strong> semesters.</p>
           <ResponsiveContainer width="100%" height="50%">
             <PieChart>
@@ -78,14 +77,14 @@ export default function OrganizationStatusPage() {
               membershipInfo.membership_points >= orgData.organization_threshold &&
 
                 <text x='50%' y="50%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
-                  <tspan x="50%" dy=".6em">Active</tspan>
-                  <tspan x="50%" dy="1.2em">Membership</tspan>
+                  <tspan x="50%" dy=".6em" fill={orgData.organization_color}>Active</tspan>
+                  <tspan x="50%" dy="1.2em" fill={orgData.organization_color}>Membership</tspan>
                 </text> 
             }
             {
                membershipInfo.membership_points < orgData.organization_threshold &&
                <text x='50%' y="40%" scaleToFit="true" textAnchor="middle" verticalAnchor="middle" textLength={160}>
-               <tspan x="50%" dy=".6em">  {(parseInt(membershipInfo.membership_points) / parseInt(orgData.organization_threshold) * 100).toFixed(0)} %</tspan>
+               <tspan x="50%"fill={orgData.organization_color}  dy=".6em">  {(parseInt(membershipInfo.membership_points) / parseInt(orgData.organization_threshold) * 100).toFixed(0)} %</tspan>
                <tspan x="50%" dy="1.2em">to Active</tspan>
                <tspan x="50%" dy="1.2em">Membership</tspan>
                </text>
